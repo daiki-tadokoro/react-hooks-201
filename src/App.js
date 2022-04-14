b3c65ea518a91ff29a1a1f3e1cc4be6b7e86ad39
田所大樹 <tadokoro@crewinc.io>
2022-04-14 13:50:17 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React, { useState } from "react";
 
 const App = (props) => {
-  // const initialStates = {
-  //   name: "",
-  //   price: 1000,
-  // };
-
-  // const [name, setName] = useState(initialStates.name);
-  // const [price, setPrice] = useState(initialStates.price);
-
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
   return (
     <>
       <p>
         現在の{name}は{price}円です。
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>Reset</button>
 
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
 
 App.defaultProps = {
-  name: "市場価格",
-  price: 100,
+  name: "",
+  price: 1000,
 };
 
 export default App;
